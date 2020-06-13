73f67649450f20c1d82d16bd27edbeafdb01a4ca
Ankush Garg <ankush.garg@iongroup.com>
2020-06-13 17:46:59 +0530

    121212

--- src/main/java/utilities/Logger.java
+++ src/main/java/utilities/Logger.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
+import org.bson.internal.Base64;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -12,18 +13,14 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.MediaEntityBuilder;
 import com.aventstack.extentreports.Status;
-import com.aventstack.extentreports.reporter.BasicFileReporter;
-import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.aventstack.extentreports.reporter.ExtentKlovReporter;
-import com.aventstack.extentreports.reporter.ExtentLoggerReporter;
 
 public class Logger {
 
 	public static ExtentReports extent = new ExtentReports();
-	public static ExtentKlovReporter klovReport = new ExtentKlovReporter("klovProjectName");
+	public static ExtentKlovReporter klov = new ExtentKlovReporter(QaEnvironment.getProperty("klov.project.name"));
 	public static ExtentTest test;
 
-	public static BasicFileReporter reporter = new ExtentLoggerReporter("basicreport.html"); 
 	/**
 	 * Take screenshot
 	 * 
@@ -44,43 +41,29 @@ public class Logger {
 		return fileWithPath;
 	}
 
+	public static String takeScreenshot(WebDriver driver) throws IOException {
+		TakesScreenshot scrShot = ((TakesScreenshot) driver);
+		File SrcFile = scrShot.getScreenshotAs(OutputType.FILE);
+		return SrcFile.getAbsolutePath();
+	}
+
 	/**
 	 * maintain report on server.
+	 * 
+	 * @throws Exception
 	 */
-	public static void initKlovReport() {	
-		// intialize with a project name, build name
-		ExtentKlovReporter klov = new ExtentKlovReporter("project", "build");
+	public static void initKlovReport() throws Exception {
 		// address, host/port of MongoDB
 		klov.initMongoDbConnection("localhost", 27017);
 		// Klov server address
 		klov.initKlovServerConnection("http://localhost");
-
-		extent.setSystemInfo("Application Name", "Yahoo Test");
-		extent.setSystemInfo("User Name", "Ankur Jain");
-		extent.setSystemInfo("Envirnoment", "Production");
-
-		extent.attachReporter(reporter, klov);
-		
-		ExtentTest test = extent.createTest("Test"); // level = 0
-		ExtentTest node = test.createNode("Node");  // level = 1
-		node.pass("details");
-
-		// short-hand
-		extent.createTest("MyFirstTest").createNode("MyFirstChildTest").pass("details");
-
-		// description
-		ExtentTest node1 = test.createNode("MyFirstChildTest", "Node Description");
-		
-		node1.pass("Yahoo");
-		
-		test.getExtent().flush();
-		//htmlReporter.config().enableTimeline(true);
-		// htmlReporter.config().setAutoCreateRelativePathMedia(true);
-		//report.attachReporter(klovReport, htmlReporter);
+		extent.setSystemInfo("User Name", QaEnvironment.getProperty("user.name"));
+		extent.setSystemInfo("Envirnoment", QaEnvironment.getProperty("environment"));
+		extent.attachReporter(klov);
 	}
 
 	public static void setProjectName(String projectName) {
-		klovReport.setProjectName(projectName);
+		klov.setProjectName(projectName);
 	}
 
 	public static void setProjectName() {
@@ -88,12 +71,12 @@ public class Logger {
 	}
 
 	public static void setReportName(String reportName) {
-		klovReport.setReportName(reportName);
+		klov.setReportName(reportName);
 	}
-	
+
 	public static void setReportName() {
 		setReportName(QaEnvironment.getProperty("klov.report.name"));
-	}	
+	}
 
 	/**
 	 * Log the test in report.
@@ -105,17 +88,35 @@ public class Logger {
 	}
 
 	public static void pass(WebDriver driver) throws IOException {
-		String screenShotPath = Logger.takeScreenshot(driver, "Pass");
+		String screenShotPath = takeScreenshot(driver);
+		String base64File = encodeFiletoBase64Binary(new File(screenShotPath));
 		test.log(Status.PASS, "Successful Booking Snapshot Below :",
-				MediaEntityBuilder.createScreenCaptureFromPath(screenShotPath).build());
+				MediaEntityBuilder.createScreenCaptureFromBase64String(base64File).build());
 		System.out.println("********* Passed *********");
 	}
 
 	public static void fail(WebDriver driver) throws IOException {
-		String screenShotPath = Logger.takeScreenshot(driver, "Fail");
+		String screenShotPath = takeScreenshot(driver);
+//		String base64File = encodeFiletoBase64Binary(new File(screenShotPath));
+//		test.log(Status.FAIL, "Failed Booking Snapshot Below :",
+//				MediaEntityBuilder.createScreenCaptureFromPath(screenShotPath).build());
+//		System.out.println(base64File);
+		String base64File = encodeFiletoBase64Binary(new File(screenShotPath));
 		test.log(Status.FAIL, "Failed Booking Snapshot Below :",
-				MediaEntityBuilder.createScreenCaptureFromPath(screenShotPath).build());
+				MediaEntityBuilder.createScreenCaptureFromBase64String(base64File).build());
+
+		test.addScreenCaptureFromBase64String(base64File, "Fail");
 		System.out.println("********* Failed *********");
+	}
+
+//	private static String getBase64EncodedFile(File file) throws IOException {
+//		return encodeFiletoBase64Binary(takeScreenshot(driver));
+//	}
+
+	private static String encodeFiletoBase64Binary(File file) throws IOException {
+		byte[] fileContent = FileUtils.readFileToByteArray(file);
+		String encodedString = Base64.encode(fileContent);
+		return encodedString;
 
 	}
 }
